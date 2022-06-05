5640140a20167098ee272d72b9d1b55a1141dddb
Hibiki <hibiki.takahashi0801@gmail.com>
2022-06-05 18:04:39 +0900

    [FrontEnd]#0003 InsertLoto6ResultsScreen

--- frontend/src/components/modules/header.vue
+++ frontend/src/components/modules/header.vue
@@ -3,6 +3,7 @@
     <b-nav-item><router-link to="/"><img src="../../assets/images/toplogo.png"></router-link></b-nav-item>
     <b-nav-item-dropdown id="loto6list" text="LOTO6" toggle-class="nav-link-custom">
       <b-dropdown-item><router-link to="/loto6results">Result</router-link></b-dropdown-item>
+      <b-dropdown-item><router-link to="/registerloto6results">Register</router-link></b-dropdown-item>
     </b-nav-item-dropdown>
     <b-nav-item>Disabled</b-nav-item>
   </b-nav>
