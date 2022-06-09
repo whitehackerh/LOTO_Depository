15f8da2dc30a9250be23c874d3bd78744c8ceaeb
Hibiki <hibiki.takahashi0801@gmail.com>
2022-06-10 00:58:56 +0900

    logo size fix

--- frontend/src/components/modules/header.vue
+++ frontend/src/components/modules/header.vue
@@ -1,6 +1,6 @@
 <template #header>
   <b-nav tabs>
-    <b-nav-item><router-link to="/"><img src="../../assets/images/toplogo.png"></router-link></b-nav-item>
+    <b-nav-item><router-link to="/"><img id="logo" src="../../assets/images/toplogo.png"></router-link></b-nav-item>
     <b-nav-item-dropdown id="loto6list" text="LOTO6" toggle-class="nav-link-custom">
       <b-dropdown-item><router-link to="/loto6results">Result</router-link></b-dropdown-item>
       <b-dropdown-item><router-link to="/registerloto6results">Register</router-link></b-dropdown-item>
@@ -19,5 +19,15 @@ export default {
 <style>
   #header {
     position: sticky;
+  }
+  #logo {
+    width: auto;
+    height: 75px;
+  }
+  #loto6list {
+    
+  }
+  #loto7list {
+
   }
 </style>
