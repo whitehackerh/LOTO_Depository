dd7bf661816771847b2082d6d63c88770f698ee5
Hibiki <hibiki.takahashi0801@gmail.com>
2022-06-09 23:02:59 +0900

    [FrontEnd]#0004 getLoto7ResultsScreen

---
+++ frontend/src/components/pages/loto7results.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <HeaderComponent />
+        <p>Results: {{results}}</p>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from "../modules/header.vue";
+import axios from 'axios';
+export default {
+    name: 'Loto7ResultsPage',
+    components: {
+        HeaderComponent
+    },
+    data() {
+        return {
+            results:""
+        }
+    },
+    mounted() {
+        this.getLoto7Results()
+    },
+    methods: {
+        async getLoto7Results() {
+            const results = await axios.get("/getLoto7Results");
+            this.results = results.data
+        }
+    }
+};
+</script>
