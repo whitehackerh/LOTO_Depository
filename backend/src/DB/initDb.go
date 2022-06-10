201e7af98efc38b76e2c007fda0b5912dd49b80d
Hibiki <hibiki.takahashi0801@gmail.com>
2022-06-11 00:51:13 +0900

    maintenance

--- backend/src/DB/initDb.go
+++ backend/src/DB/initDb.go
@@ -6,7 +6,7 @@ var Db *sql.DB
 
 func ConnectDb() *sql.DB {
 	var err error
-	Db, err = sql.Open("postgres", "user=postgres dbname=loto_depository password=ash0001 sslmode=disable")
+	Db, err = sql.Open("postgres", "user=postgres dbname=loto_depository password=postgres sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
