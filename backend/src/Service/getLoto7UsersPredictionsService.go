7c52fc37eef853f3b186471ee359bab04dc8b71d
Hibiki <hibiki.takahashi0801@gmail.com>
2022-08-20 22:57:40 +0900

    improve

--- backend/src/Service/getLoto7UsersPredictionsService.go
+++ backend/src/Service/getLoto7UsersPredictionsService.go
@@ -10,6 +10,8 @@ func GetLoto7UsersPredictions(body map[string]string) map[int]map[string]interfa
 	user_id, _ := strconv.Atoi(body["user_id"])
 	records := model.GetLoto7UsersPredictions(user_id)
 	results := model.GetLoto7Results()
+	latestResult := model.GetNewestLoto7Result()
+	beforeResultAnnouncementCount := 0
 
 	data := make(map[int]map[string]interface{})
 	if records[0].Time == 0 {
@@ -18,6 +20,9 @@ func GetLoto7UsersPredictions(body map[string]string) map[int]map[string]interfa
 		data[0]["Rate"] = 0.0
 		data[0]["Average"] = 0.0
 		data[0]["Time"] = 0
+		data[0]["existBeforeResultAnnouncement"] = false
+		data[0]["existAfterResultAnnouncement"] = false
+		data[0]["beforeResultAnnouncementCount"] = 0
 		return data
 	}
 
@@ -37,8 +42,18 @@ func GetLoto7UsersPredictions(body map[string]string) map[int]map[string]interfa
 
 	counter2 := 0
 	total := 0
+	data[0]["existAfterResultAnnouncement"] = false
+	data[0]["existAfterResultAnnouncement"] = false
 	for i := 0; i < len(data); i++ {
 		data[counter2]["Matches"] = 0
+		data[counter2]["beforeResultAnnouncement"] = false
+		if data[counter2]["Time"].(int) > latestResult[0].Time {
+			beforeResultAnnouncementCount++
+			data[counter2]["beforeResultAnnouncement"] = true
+			counter2++
+			continue
+		}
+		data[0]["existAfterResultAnnouncement"] = true
 		for _, value := range results {
 			if data[counter2]["Time"] == value.Time {
 				if data[counter2]["Number_1"] == value.Number_1 {
@@ -241,10 +256,22 @@ func GetLoto7UsersPredictions(body map[string]string) map[int]map[string]interfa
 		}
 		counter2++
 	}
+	data[0]["beforeResultAnnouncementCount"] = beforeResultAnnouncementCount
+	if beforeResultAnnouncementCount > 0 {
+		data[0]["existBeforeResultAnnouncement"] = true
+	} else {
+		data[0]["existBeforeResultAnnouncement"] = false
+	}
 	data[0]["Total"] = total
-	data[0]["Records"] = len(data)
-	data[0]["Average"] = float64(total) / float64(len(data))
-	data[0]["Rate"] = float64(total) / (float64(len(data)) * 7) * 100
-
+	data[0]["Records"] = len(data) - beforeResultAnnouncementCount
+	if data[0]["Records"].(int) <= 0 {
+		data[0]["Average"] = 0.0
+		data[0]["Rate"] = 0.0
+		data[0]["existAfterResultAnnouncement"] = false
+		return data
+	}
+	data[0]["Average"] = float64(total) / (float64(len(data)) - float64(beforeResultAnnouncementCount))
+	data[0]["Rate"] = float64(total) / ((float64(len(data)) - float64(beforeResultAnnouncementCount)) * float64(7)) * float64(100)
+	data[0]["existAfterResultAnnouncement"] = true
 	return data
 }
--- frontend/src/components/pages/usersPredictionsLoto7.vue
+++ frontend/src/components/pages/usersPredictionsLoto7.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
     <HeaderComponent />
+        <div class="beforeResultAnnouncementTable" v-show="predictions[0].Time!=0 && predictions[0].existBeforeResultAnnouncement==true">
+        <h3>Before Result Announcement</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th id="time">Time</th>
+                    <th colspan="7">Numbers</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in predictions" v-show="row.beforeResultAnnouncement==true" :key="row.Time">
+                    <td id="LinkToDetail" @click="movePredictionsDetailLoto6(row.Time)">{{row.Time}}</td>
+                    <td>{{row.Number_1}}</td>
+                    <td>{{row.Number_2}}</td>
+                    <td>{{row.Number_3}}</td>
+                    <td>{{row.Number_4}}</td>
+                    <td>{{row.Number_5}}</td>
+                    <td>{{row.Number_6}}</td>
+                    <td>{{row.Number_7}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <div class="usersPredictionsTable" v-show="predictions[0].Time!=0">
         <h3 id="Records">Records : {{predictions[0].Records}}</h3>
         <h3 id="Average">Average : {{Math.floor(predictions[0].Average * 1000) / 1000}}</h3>
@@ -14,7 +37,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="row in predictions"  :key="row.Time">
+                                <tr v-for="row in predictions" v-show="row.beforeResultAnnouncement==false" :key="row.Time">
                     <td id="LinkToDetail" @click="movePredictionsDetailLoto7(row.Time)">{{row.Time}}</td>
                     <td>{{row.Number_1}}</td>
                     <td>{{row.Number_2}}</td>
@@ -80,6 +103,17 @@ export default {
     background: #66CCFF;
 }
 .usersPredictionsTable td {
+    border: 1px solid #000066;
+    background: #ffffff;
+}
+.beforeResultAnnouncementTable th,td {
+    width: 100px;
+}
+.beforeResultAnnouncementTable th {
+    border: 1px solid #000066;
+    background: #66CCFF;
+}
+.beforeResultAnnouncementTable td {
     border: 1px solid #000066;
     background: #ffffff;
 }
